9d66eeef5e5cacf29ea2cfccbbf31a5f295f9c0b
nazguul <103865507+nzgl-g@users.noreply.github.com>
2025-05-01 19:35:33 +0100

    r

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -8,7 +8,6 @@ const voteRouter = require("../routes/vote");
 const invitationRouter = require("../routes/invitation");
 const teamRouter = require("../routes/team");
 const taskRouter = require("../routes/task");
-const blockchainRouter = require("../routes/blockchain");
 
 module.exports = function (app) {
   app.use("/votex/api/signup", signupRouter);
@@ -20,6 +19,5 @@ module.exports = function (app) {
   app.use("/votex/api/invitations", invitationRouter);
   app.use("/votex/api/sessions/:sessionId/vote", voteRouter);
   app.use("/votex/api/sessions/:sessionId/candidate", candidateRouter);
-  app.use("/votex/api/blockchain", blockchainRouter);
 };
 //dont bother with this file . it just makes the routes work
